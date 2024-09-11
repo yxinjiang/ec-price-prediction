0fe56865ac36cd7d82f17d4b7b30c25d4912bb71
yxinjiang <yxinjiang@gmail.com>
2024-09-11 16:40:14 +0800

    update readme

--- main.py
+++ main.py
@@ -3,22 +3,29 @@ from prophet.serialize import model_from_json
 import joblib
 import pandas as pd 
 
-
 app = FastAPI()
 
-@app.post('/get_predictions')
-async def get_predictions(area:int,year_after_lease_com:int,date_start:str,date_end:str):
-    area =int(area)
+@app.post('/predict_price')
+async def predict_price(area:float,floor_range:str,district:str,market_segment:str,year:int,month:int,year_after_lease_com:int):
+    area =float(area)
+    floor_range = float(floor_range)
+    district = str(district)
+    market_segment = str(market_segment)
+    year = int(year)
+    month = int(month)
     year_after_lease_com =int(year_after_lease_com)
-    start_date =str(date_start)
-    end_date =str(date_end)
-    scaler = joblib.load('./models/KBinsDiscretizer.save') 
-    area_bin = int(scaler.transform([[area]])[0][0])
-    with open(f'./models/model_area{area_bin}_year{year_after_lease_com}.json', 'r') as fin:
+    with open(f'./models/models.pkl', 'r') as fin:
         model = model_from_json(fin.read())  # Load model
-    ds = pd.date_range(start=start_date,end=end_date,freq='MS')
-    df_test = pd.DataFrame(ds,columns=['ds'])
-    predict = model.predict(df_test).loc[:, 'yhat'].to_frame().to_json()
+    data = pd.DataFrame([{'area': area,
+                        'floor_range': floor_range,
+                        'district': district,
+                        'market_segment': market_segment,
+                        'year': year,
+                        'month': month,
+                        'year_after_lease_com': year_after_lease_com}
+                        ])
+                        
+    predict = model.predict(data).loc[:, 'yhat'].to_frame().to_json()
     response = {
         'Prediction':predict,
                 }
